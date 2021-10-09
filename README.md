90530c3631b7bb5572829cf598be495844bf52df
1Michael23 <michael.lagienstra0@gmail.com>
2021-10-09 16:27:50 +1100

    1.0

---
+++ README.md
@@ -0,0 +1,3 @@
+# Oracle-rs
+
+A recreation of the Oracle from TempleOS, by Terry A Davis
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,54 @@
-use std::fmt::Result;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::iter::FromIterator;
-use std::string;
+extern crate rand;
 
+use rand::seq::IteratorRandom; // 0.7.3
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+extern crate clap;
+use clap::{Arg, App};
+
+const FILENAME: &str = "wordlist.txt";
 fn main() {
-    let filename = "wordlist.txt";
-    // Open the file in read-only mode (ignoring errors).
-    let file = File::open(filename).unwrap();
-    let reader = BufReader::new(file);
+    
+   
 
+    //Set options for cli arguments
 
-    for  (count,line) in reader.lines().enumerate() {       
-        println!("{}", line.unwrap())
+    let cli = App::new("Oracle-rs")
+        .version("1.0")
+        .author("github.com/1Michael23/oracle-rs")
+        .about("A recreation of the Oracle by Terry A Davis.")
+            .arg(Arg::new("quantity")
+                .short('q')
+                .about("The number of words to print.")
+                .default_value("1"))
+        .get_matches();
+
+        let string = cli.value_of("quantity").unwrap().to_string();
+        let quantity: u8 = string.parse().unwrap();
+
+        for _i in 0..quantity {
+            let output = find_word();
+            print!("{}", output);
+            print!(" ")
+        }
+        
     }
+
+// find a line from the specified file
+
+fn find_word() -> String {
+    let f = File::open(FILENAME)
+        .unwrap_or_else(|e| panic!("(;_;) file not found: {}: {}", FILENAME, e));
+    let f = BufReader::new(f);
+
+    let lines = f.lines().map(|l| l.expect("Couldn't read line"));
+
+    lines
+        .choose(&mut rand::thread_rng())
+        .expect("File had no lines")
+
+
 }
